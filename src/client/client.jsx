8d0c69df9a593513f64c316b4725a2015e1c4d52
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-28 22:06:14 +0900

    hydrate client side js code

---
+++ src/client/client.jsx
@@ -0,0 +1,5 @@
+import React from "https://jspm.dev/react@17.0.2";
+import ReactDOM from "https://jspm.dev/react-dom@17.0.2";
+import { App } from "./App.jsx"
+
+ReactDOM.hydrate(<App/>, document.getElementById("app"))
